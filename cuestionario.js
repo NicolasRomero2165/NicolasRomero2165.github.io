5e27c7261cac56e2851d491cb94c17d496dd5ce4
Rodrigo Nicolás Romero <123268630+NicolasRomero2165@users.noreply.github.com>
2024-10-27 17:37:27 -0300

    Delete cuestionario.js

--- cuestionario.js
+++
@@ -1,5 +0,0 @@
-var nombre = prompt("¿Cúal es su nombre, Marine?");
-var apellido = prompt("¿Su apellido?");
-var sexo = prompt("¿Cuál es su sexo?");
-var edad = prompt("¿Cuántos años tiene?");
-var arma = prompt("¿Su arma favorita?");
